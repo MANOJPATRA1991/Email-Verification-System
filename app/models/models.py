760372673e0e0c24835d8d391631b0fed226356b
MANOJ PATRA <patra.manoj0@gmail.com>
2017-09-11 18:23:06 +0530

    Add User model

---
+++ app/models/models.py
@@ -0,0 +1,24 @@
+from sqlalchemy import (Column, ForeignKey, Integer,
+                        String, Boolean, DateTime, func)
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email_id = Column(String(150), unique=True, nullable=False)
+    verification_code = Column(String(150), nullable=False)
+    is_verified = Column(Boolean, default=False)
+    created_at = Column(DateTime, nullable=True)
+    updated_at = Column(DateTime, nullable=True)
+
+engine = create_engine('postgresql://dbuser:users@localhost/users')
+
+
+Base.metadata.create_all(engine)
